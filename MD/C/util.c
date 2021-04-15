92efcd9748cd7add5ae7077ed0045873d0938f3b
okapeng <okapengwyq@gmail.com>
2021-04-15 14:09:10 +0100

    add restrict to util.c

--- MD/C/util.c
+++ MD/C/util.c
@@ -1,20 +1,20 @@
 #include <math.h>
 
-void vis_forces(int N,double *f, double *vis, double *velo)
+void vis_forces(int N,double *f, double* restrict vis, double* restrict velo)
 {
   int i;
           for(i=0;i<N;i++){
             f[i] = -vis[i] * velo[i];
           }
 }
-void wind_force(int N,double *f, double *vis, double velo)
+void wind_force(int N,double *f, double* restrict vis, double velo)
 {
   int i;
           for(i=0;i<N;i++){
             f[i] = f[i] -vis[i] * velo;
           }
 }
-void add_norms(int N,double *r, double *delta)
+void add_norms(int N,double *r, double* restrict delta)
 {
   int k;
         for(k=0;k<N;k++){
