d1d94ef3925311e4dffb883d71c77756d927e6c0
okapeng <okapengwyq@gmail.com>
2021-04-15 18:05:44 +0100

    remove parallel for util.c

--- MD/C/util.c
+++ MD/C/util.c
@@ -1,10 +1,8 @@
 #include <math.h>
-#include <omp.h>
 
 void vis_forces(int N,double *f, double* restrict vis, double* restrict velo)
 {
   int i;
-  #pragma omp parallel for
           for(i=0;i<N;i++){
             f[i] = -vis[i] * velo[i];
           }
@@ -12,7 +10,6 @@ void vis_forces(int N,double *f, double* restrict vis, double* restrict velo)
 void wind_force(int N,double *f, double* restrict vis, double velo)
 {
   int i;
-  #pragma omp parallel for
           for(i=0;i<N;i++){
             f[i] = f[i] -vis[i] * velo;
           }
@@ -20,7 +17,6 @@ void wind_force(int N,double *f, double* restrict vis, double velo)
 void add_norms(int N,double *r, double* restrict delta)
 {
   int k;
-  #pragma omp parallel for
         for(k=0;k<N;k++){
           r[k] += (delta[k] * delta[k]);
         }
